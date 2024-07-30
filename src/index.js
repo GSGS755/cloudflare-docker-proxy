1c0c33f318fbaee76f32a073547ff1af96a1b347
GSGS755 <52650630+GSGS755@users.noreply.github.com>
2024-07-30 13:06:28 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ttcgsc.tk": dockerHub,
+  "quay.ttcgsc.tk": "https://quay.io",
+  "gcr.ttcgsc.tk": "https://gcr.io",
+  "k8s-gcr.ttcgsc.tk": "https://k8s.gcr.io",
+  "k8s.ttcgsc.tk": "https://registry.k8s.io",
+  "ghcr.ttcgsc.tk": "https://ghcr.io",
+  "cloudsmith.ttcgsc.tk": "https://docker.cloudsmith.io",
+  "ecr.ttcgsc.tk": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ttcgsc.tk": dockerHub,
 };
 
 function routeByHosts(host) {
